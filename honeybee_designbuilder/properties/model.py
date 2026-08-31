b7e770d60dd30951696023f73bf519bdbab2d213
Chris Mackey <chris@ladybug.tools>
2026-08-31 14:01:45 -0700

    fix(model): Check degeneracy at model tolerance

--- honeybee_designbuilder/properties/model.py
+++ honeybee_designbuilder/properties/model.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 """Model DesignBuilder Properties."""
-from honeybee.units import parse_distance_string
 
 
 class ModelDesignBuilderProperties(object):
@@ -44,7 +43,6 @@ class ModelDesignBuilderProperties(object):
         msgs = []
         tol = self.host.tolerance
         ang_tol = self.host.angle_tolerance
-        e_tol = parse_distance_string('0.1m', self.host.units)
 
         # perform checks for duplicate identifiers, which might mess with other checks
         msgs.append(self.host.check_all_duplicate_identifiers(False, detailed))
@@ -52,7 +50,7 @@ class ModelDesignBuilderProperties(object):
         # perform several checks for the Honeybee schema geometry rules
         msgs.append(self.host.check_planar(tol, False, detailed))
         msgs.append(self.host.check_self_intersecting(tol, False, detailed))
-        msgs.append(self.host.check_degenerate_rooms(e_tol, False, detailed))
+        msgs.append(self.host.check_degenerate_rooms(tol, False, detailed))
 
         # perform geometry checks related to parent-child relationships
         msgs.append(self.host.check_sub_faces_valid(tol, ang_tol, False, detailed))
